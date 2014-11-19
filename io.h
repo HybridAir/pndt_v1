5a1dd3b063c07b87b6fb8061c6dfa52231159471
Blake <Blake@AR1>
2014-11-19 08:00:51 -0700

    Added pin defines

--- io.h
+++ io.h
@@ -3,4 +3,14 @@
 
 #include <Arduino.h>
 
+#define BTNL        10                                                          //left button
+#define BTNC        9                                                           //center button
+#define BTNR        8                                                           //right button
+#define TMP         A5                                                          //temperature pin
+
+#define OFF         1                                                           //off pin
+#define ON          0                                                           //on pin
+#define CHRG        5                                                           //charge status pin
+#define BATT        A0                                                          //battery voltage pin
+
 #endif
