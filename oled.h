6a03239a4c93019d15863f846e67e95904c637a7
HybridAir <s.tratton.blake@gmail.com>
2014-11-23 11:41:25 -0700

    I need to make more commits, netbeans thinks I changed everything here and I probably did

--- oled.h
+++ oled.h
@@ -1,7 +1,28 @@
 #ifndef OLED_H
 #define	OLED_H
 
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
 
+#include "io.h"
+#include "rtcTime.h"
+
+
+#define OLED_DC     4
+#define OLED_CS     6
+#define OLED_RESET  12
+#define OLED_WIDTH  128
+
+
+class oled {
+    public:
+        oled();
+        void begin();
+        void dispMon();
+              
+};
 
 #endif
 
