db3f80d2f6dd87ffe596968f5de3f67d82116553
HybridAir <s.tratton.blake@gmail.com>
2014-11-23 12:07:36 -0700

    Got the debug page sort of working

--- pages.cpp
+++ pages.cpp
@@ -2,10 +2,36 @@
 
 #include "pages.h"
 
+extern io inout;
+extern rtcTime time;
+extern Adafruit_SSD1306 display;
+
 pages::pages() {
     
 }
 
 void pages::debug() {
+        display.setCursor(0, 0);
+    display.setTextSize(1);
+    display.println(inout.getBatt());
+    display.println(inout.getTmp());
+    display.println(inout.btnMon());
     
+    
+    display.println(RTC.get());
+    display.println(now());
+    display.println(time.getDate());
+    //display.println(year());
+    display.println(time.getTime());
+    if(inout.monitorBatt() == 1) {
+        display.setTextSize(1);
+        display.println("low batt");
+    }
+    else if(inout.monitorBatt() == 2) {
+        display.setTextSize(1);
+        display.println("batt depleted");
+        display.display();
+        delay(2000);
+        inout.turnOff();
+    }
 }
