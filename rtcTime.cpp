e0a4964c885f8b734b0d9778f7b57222b7d719cd
HybridAir <s.tratton.blake@gmail.com>
2014-11-22 18:57:25 -0700

    Basic time stuff

--- rtcTime.cpp
+++ rtcTime.cpp
@@ -1,1 +1,37 @@
 //manages all rtc time related things
+
+#include "rtcTime.h"
+
+rtcTime::rtcTime() {                                                                      //default constructor
+    //RTC.set(1416113806);
+     // the function to get the time from the RTC
+        
+      //setTime(1416113806);
+    //RTC.set(1416113806);
+    
+    //setTime(1416113806);
+    //RTC.set(now());
+      
+      
+
+}
+
+String rtcTime::getTime() {
+    String out;
+    out.concat(hour());
+    out.concat(":");
+    out.concat(minute());
+    out.concat(":");
+    out.concat(second());
+    return out;
+}
+
+String rtcTime::getDate() {
+    String out;
+    out.concat(month());
+    out.concat(" ");
+    out.concat(day());
+    //out.concat(" ");
+    //out.concat(year());
+    return out;
+}
