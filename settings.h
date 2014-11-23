d88ec8a1ed0d9b1a5337f960ab78f4d77a7c5929
HybridAir <s.tratton.blake@gmail.com>
2014-11-23 13:16:00 -0700

    Added page function definitions

--- settings.h
+++ settings.h
@@ -1,7 +1,20 @@
 #ifndef SETTINGS_H
 #define	SETTINGS_H
 
+#include <Arduino.h>
+#include <EEPROM.h>
 
+class settings {
+    public:
+        settings();
+        byte getrom();
+        byte getPage();
+        void setPage(byte in);
+        void nextPage();
+        void prevPage();
+private:
+    byte mode;
+};
 
 #endif
 
