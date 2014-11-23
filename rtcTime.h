a1c22525c05886357009216fb7f6459aac4aa122
HybridAir <s.tratton.blake@gmail.com>
2014-11-22 18:57:29 -0700

    Basic time stuff

--- rtcTime.h
+++ rtcTime.h
@@ -1,7 +1,18 @@
 #ifndef RTCTIME_H
 #define	RTCTIME_H
 
+#include <Arduino.h>
+#include <Time.h>
+#include <MCP79412RTC.h>
+#include <Wire.h>
 
+class rtcTime {
+    public:
+        rtcTime();
+        String getTime();
+        String getDate();
+    private:
+};
 
 #endif
 
