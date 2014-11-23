5008c50bd5f412ae537bbfd39f340ae7f2cd79c3
HybridAir <s.tratton.blake@gmail.com>
2014-11-23 12:37:02 -0700

    Added btnbar code

--- oled.cpp
+++ oled.cpp
@@ -20,7 +20,7 @@ pages page;
 oled::oled() {   
   display.setTextSize(4);
   display.setTextColor(WHITE);
-  display.setTextWrap(true);
+  display.setTextWrap(false);
   
   
 }
@@ -42,6 +42,40 @@ void oled::dispMon() {
     //includes the button bar and probably battery warnings
     //last line is a display.display
     display.clearDisplay();
-    page.debug();
+    //page.debug();
+    drawBtnBar();
+    page.scrollText();
+    
     display.display();
+}
+
+void oled::drawBtnBar() {
+    display.drawFastHLine(0, 55, display.width(), WHITE);
+    display.setTextSize(1);
+    
+    //left
+    display.fillTriangle(8, 56, 16, 56, 8, 64, WHITE);
+    display.fillRect(0, 56, 8, 8, WHITE);
+    
+    display.setTextColor(BLACK);
+    display.setCursor(2, 56);
+    display.print("<");
+    display.setCursor(3, 56);
+    display.print("<");
+    
+    //center
+    display.setTextColor(WHITE);
+    display.setCursor(52, 56);
+    display.print("Menu");
+    
+    //right
+    display.fillTriangle(112, 56, 120, 56, 120, 64, WHITE);
+    display.fillRect(120, 56, 8, 8, WHITE);
+    
+    display.setTextColor(BLACK);
+    display.setCursor(121, 56);
+    display.print(">");
+    display.setCursor(120, 56);
+    display.print(">");
+    //display.display();
 }
