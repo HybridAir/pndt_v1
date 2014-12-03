275b44f9cda25d880a90c6b6e109f43c677fcd5e
HybridAir <s.tratton.blake@gmail.com>
2014-12-02 18:47:36 -0700

    Added basic menu functionality

--- menus.cpp
+++ menus.cpp
@@ -2,11 +2,17 @@
 
 #include "menus.h"
 
+extern Adafruit_SSD1306 display;
+
 menus::menus() {
     //defaults 
 
 }
 
 void menus::doMain() {
-
+    display.setTextColor(WHITE);
+    display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println("Main Menu");
+  display.display();
 }
