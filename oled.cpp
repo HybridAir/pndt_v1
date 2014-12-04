bb4ab3c1991ea23d2c8d5f5cd632dd677b46497c
HybridAir <s.tratton.blake@gmail.com>
2014-12-03 17:03:28 -0700

    Moved button bar code back here

--- oled.cpp
+++ oled.cpp
@@ -8,14 +8,13 @@ extern menus menu;
 //extern rtcTime time;
 
 extern Adafruit_SSD1306 display;
+extern pages page;
 
 //function that is called all the time in main to update the display
 //"page" functionality
 //page/mode selection menu
 //scrolling text announcement page, home screen clock data thing, battery/charge, settings, games, power warnings
 //Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
-
-pages page;
 
 
 oled::oled() {   
@@ -53,4 +52,35 @@ void oled::dispMon() {
     }
     
     display.display();
+}
+
+void oled::drawBtnBar(char left, char right) {
+    display.drawFastHLine(0, 55, display.width(), WHITE);
+    display.setTextSize(1);
+    
+    //left
+    display.fillTriangle(8, 56, 16, 56, 8, 64, WHITE);
+    display.fillRect(0, 56, 8, 8, WHITE);
+    
+    display.setTextColor(BLACK);
+    display.setCursor(2, 56);
+    display.print(left);
+    display.setCursor(3, 56);
+    display.print(left);
+    
+    //center
+    display.setTextColor(WHITE);
+    display.setCursor(52, 56);
+    display.print("Menu");
+    
+    //right
+    display.fillTriangle(112, 56, 120, 56, 120, 64, WHITE);
+    display.fillRect(120, 56, 8, 8, WHITE);
+    
+    display.setTextColor(BLACK);
+    display.setCursor(121, 56);
+    display.print(right);
+    display.setCursor(120, 56);
+    display.print(right);
+    //display.display();
 }
