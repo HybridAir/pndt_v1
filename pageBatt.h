f36bb4662d46527cbe986612a1e7e653f75996d7
HybridAir <s.tratton.blake@gmail.com>
2014-12-05 15:59:26 -0700

    Added battery warning code

--- pageBatt.h
+++ pageBatt.h
@@ -4,21 +4,24 @@
 #include <Arduino.h>
 #include "oled.h"
 
+#define WARNDELAY 10000
+
 class pageBatt {
     public:
         pageBatt();
         void showPage(bool newPage);
         String getTitle();
-        void doPage();
+        void powerCheck();
         void doBtn(byte btn);
     private:       
         void resetVars();
         void charge();
-        void drawBattery(byte x, byte y);
+        void drawBattery(byte x, byte y, bool dead);
         void drawBattBar(byte x, byte y, byte bar);
-        
+        void battWarn(bool dead);       
         unsigned long previousMillis; 
         bool barActive;
+        bool justReset;
               
 };
 
