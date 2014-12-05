fc41f67f146d846111d13d364b78347e6b92d208
HybridAir <s.tratton.blake@gmail.com>
2014-12-05 11:27:48 -0700

    Initial creation

---
+++ pageText.h
@@ -0,0 +1,16 @@
+#ifndef PAGETEXT_H
+#define	PAGETEXT_H
+
+#include <Arduino.h>
+#include "oled.h"
+
+class pageText {
+    public:
+        pageText();
+        void showPage();
+    private:       
+              
+};
+
+#endif
+
