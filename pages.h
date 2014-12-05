091812473716889682732283dbad84dcc7bbf4be
HybridAir <s.tratton.blake@gmail.com>
2014-12-05 11:18:18 -0700

    Moved battery page code from here

--- pages.h
+++ pages.h
@@ -3,10 +3,11 @@
 
 #include <Arduino.h>
 #include "oled.h"
-
 #include "io.h"
 #include "rtcTime.h"
 #include "settings.h"
+
+#include "pageBatt.h"
 
 #define BARDELAY 1000
 #define TITLEDELAY 1000
@@ -19,17 +20,17 @@ class pages {
     private:
         void debug();
         void scrollText();        
-        void charge();
-        void drawBattery(byte x, byte y);
-        void drawBattBar(byte x, byte y, byte bar);
+        //void charge();
+        //void drawBattery(byte x, byte y);
+        //void drawBattBar(byte x, byte y, byte bar);
         void doTitle(byte page);
         int x;
         byte prevPage;
         bool showPage;
         
-        unsigned long previousMillis; 
+        //unsigned long previousMillis; 
         unsigned long previousMillis2;
-        bool barActive;
+        //bool barActive;
               
 };
 
