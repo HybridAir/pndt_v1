7aea5a600b9ac8e683022828d8e95d9dde7469ea
HybridAir <s.tratton.blake@gmail.com>
2014-12-05 13:05:40 -0700

    Added variable resetting code and cleaned up code

--- pages.cpp
+++ pages.cpp
@@ -14,46 +14,44 @@ pageBatt batt;
 pageText text;
 
 pages::pages() {
-   // x = 130;
-    previousMillis2 = 0;                                                         //stores the last time the battery average was taken
-    prevPage = set.getPage();                                                //set the first current page, it will be the default in settings
+    prevPage = set.getPage();                                                   //gets the default first page from settings
     showPage = true;                                                            //don't need to show the title page upon power up
 }
 
-void pages::doPage() {                                                          //***checks with page needs to be displayed, and prepares to display it
+void pages::doPage() {                                                          //***manages title and page display
     byte currentPage = set.getPage();                                           //get the current page   
     if(currentPage != prevPage) {                                               //if the page has changed since it was last checked      
         prevPage = currentPage;                                                 //save the new page       
-        previousMillis2 = millis();                                             //reset the title timer
+        prevTitleTime = millis();                                               //reset the title timer
         showPage = false;                                                       //need to show the title now
+        newPage = true;
     }
     if(!showPage) {                                                             //if the title needs to be shown
-        doTitle(currentPage);                                                   //show the title for the current page   
+        doTitle(currentPage);                                                   //show the current page's title  
         
         //check the title delay timer
-        unsigned long currentMillis = millis();                                 //get the current time
-        if(currentMillis - previousMillis2 > TITLEDELAY) {                      //if we have looked at the title long enough
+        unsigned long currentTitleTime = millis();                              //save the current time
+        if(currentTitleTime - prevTitleTime > TITLEDELAY) {                     //if we have looked at the title long enough
             showPage = true;                                                    //the page can be shown now
         }
     }
     else {                                                                      //the page can be shown
         switch(currentPage) {                                                   //show the specific page
             case 0:
-                //scrollText();
-                text.showPage();
+                text.showPage(newPage);
                 break;
             case 1:
                 debug();
                 break;
             case 2:
-                //charge();
-                batt.showPage();
+                batt.showPage(newPage);
                 break;
         }
+        newPage = false;
     }
 }
 
-void pages::doTitle(byte page) {            //used to display a page's title before displaying the page
+void pages::doTitle(byte page) {                                                //used to display a page's title before displaying the page
     disp.drawBtnBar('<', "Menu", '>');
     display.setTextColor(WHITE);
     display.setTextSize(2);
@@ -62,30 +60,16 @@ void pages::doTitle(byte page) {            //used to display a page's title bef
     //the following switch case will be replaced with the page giving it its title or whatever
     switch(page) {               //show the page
         case 0:
-            display.println("0 scrolltext");
+            display.println(text.getTitle());
             break;
         case 1:
-            display.println("1 debug");
+            display.println("debug");
             break;
         case 2:
-            display.println("2 charge");
+            display.println(batt.getTitle());
             break;
     }
 }
-
-//void pages::scrollText() {
-//    disp.drawBtnBar('<', "Menu", '>');
-//    display.setTextColor(WHITE);
-//    display.setTextSize(7);
-//      x = x - 1;
-//  display.setCursor(x, 0);
-//  display.println("Hello, world.");
-//  display.display();
-//  if(x < -650) {
-//    x = 130;
-//  }
-//  
-//}
 
 void pages::debug() {
         display.setCursor(0, 0);
