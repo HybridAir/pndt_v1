c813e6a40f4b19abd5fd33221f671139e453a9e2
HybridAir <s.tratton.blake@gmail.com>
2014-12-05 11:17:56 -0700

    Moved battery page code to here

--- pageBatt.h
+++ pageBatt.h
@@ -1,7 +1,24 @@
 #ifndef PAGEBATT_H
 #define	PAGEBATT_H
 
+#include <Arduino.h>
+#include "oled.h"
 
+class pageBatt {
+    public:
+        pageBatt();
+        void showPage();
+        void doPage();
+        void doBtn(byte btn);
+    private:       
+        void charge();
+        void drawBattery(byte x, byte y);
+        void drawBattBar(byte x, byte y, byte bar);
+        
+        unsigned long previousMillis; 
+        bool barActive;
+              
+};
 
 #endif
 
