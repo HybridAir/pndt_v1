ae9da272aba029b6d0f76102516bd04d4a77dc85
HybridAir <s.tratton.blake@gmail.com>
2014-12-05 11:34:32 -0700

    Moved text scrolling page code to here

--- pageText.cpp
+++ pageText.cpp
@@ -1,3 +1,29 @@
 //handles the text scrolling page
 
 #include "pageText.h"
+
+extern settings set;
+extern oled disp;
+extern Adafruit_SSD1306 display;
+
+pageText::pageText() {
+    position = 130;
+}
+
+void pageText::showPage() {
+    scrollText();
+}
+
+void pageText::scrollText() {
+    disp.drawBtnBar('<', "Menu", '>');
+    display.setTextColor(WHITE);
+    display.setTextSize(7);
+      position = position - 1;
+  display.setCursor(position, 0);
+  display.println("Hello, world.");
+  display.display();
+  if(position < -650) {
+    position = 130;
+  }
+  
+}
