f38ba7f18737fad2557562f47db3599689b639bb
HybridAir <s.tratton.blake@gmail.com>
2014-12-05 10:15:10 -0700

    Added basic page title functionality

--- pages.h
+++ pages.h
@@ -9,6 +9,7 @@
 #include "settings.h"
 
 #define BARDELAY 1000
+#define TITLEDELAY 1000
 
 class pages {
     public:
@@ -21,9 +22,13 @@ class pages {
         void charge();
         void drawBattery(byte x, byte y);
         void drawBattBar(byte x, byte y, byte bar);
+        void doTitle(byte page);
         int x;
+        byte prevPage;
+        bool showPage;
         
         unsigned long previousMillis; 
+        unsigned long previousMillis2;
         bool barActive;
               
 };
