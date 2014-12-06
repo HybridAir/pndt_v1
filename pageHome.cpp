1124833489f45bbb683f2cbe9ac3b7a75080d2f6
HybridAir <s.tratton.blake@gmail.com>
2014-12-05 18:56:51 -0700

    Initial creation

---
+++ pageHome.cpp
@@ -0,0 +1,20 @@
+//handles the home info page
+
+#include "pageHome.h"
+
+extern settings set;
+extern oled disp;
+extern io inout;
+extern Adafruit_SSD1306 display;
+
+pageHome::pageHome() {
+
+}
+
+void pageHome::showPage(bool newPage) {
+    
+}
+
+String pageHome::getTitle() {
+    
+}
