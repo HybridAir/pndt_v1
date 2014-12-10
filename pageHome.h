b8b66a2a7d543f4183c170016c6b31ef42523721
HybridAir <s.tratton.blake@gmail.com>
2014-12-10 15:40:24 -0700

    Added battery bar drawing code

--- pageHome.h
+++ pageHome.h
@@ -8,6 +8,8 @@
 #define FONTWIDTH 6
 #define MAXDATA 2
 #define DATAHEIGHT 18
+#define DATADELAY 5
+#define BARWIDTH 8
 
 
 class pageHome {
@@ -16,16 +18,20 @@ class pageHome {
         void showPage(bool newPage);
         String getTitle();
     private:    
+        void updateVars();
         byte x;
         byte y;
         byte currentData;
         bool setNewData;
         byte newData;
-        int dataPos;
+        byte dataPos;
+        byte prevSec;
+        byte dataDelay;
         bool scrollingOut;
-        unsigned long previousMillis;
+        //unsigned long previousMillis;
         void scrollData(byte x, byte y);
         void drawData(byte x, byte y, byte data);
+        void drawBattBar(byte x, byte y, byte bar);
               
 };
 
