0c5c0f409814c4b8d8feb643a33423e0b5caf138
HybridAir <s.tratton.blake@gmail.com>
2014-12-11 09:23:02 -0700

    Initial creation

---
+++ menuTime.h
@@ -0,0 +1,18 @@
+#ifndef MENUTIME_H
+#define	MENUTIME_H
+
+#include <Arduino.h>
+#include "oled.h"
+#include "settings.h"
+
+
+class menuTime {
+    public:
+        menuTime();
+        void doMenu();
+    private:
+
+};
+
+#endif
+
