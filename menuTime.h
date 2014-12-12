39085d94fe77cd1231f6416e53181417031a2b58
HybridAir <s.tratton.blake@gmail.com>
2014-12-12 10:24:25 -0700

    Got time setting working

--- menuTime.h
+++ menuTime.h
@@ -4,13 +4,30 @@
 #include <Arduino.h>
 #include "oled.h"
 #include "settings.h"
+#include "menus.h"
+
+#define MAXUNITS 4
+#define MAXHOUR 12
+#define MAXMIN 59
 
 
 class menuTime {
+    
     public:
         menuTime();
-        void doMenu();
+        void doMenu(byte btn);
+        String getTitle();
+        
     private:
+        void doBtn();
+        void doTime();
+        void doDate();
+        void finishTime();
+        String title;
+        byte currentPage;
+        byte currentBtn;
+        byte currentTime;
+        byte activeUnit;
 
 };
 
