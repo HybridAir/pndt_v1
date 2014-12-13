adef45894c041fceb82b91115d0f9bd5592fa256
HybridAir <s.tratton.blake@gmail.com>
2014-12-12 20:43:28 -0700

    Added time sensitive messages

--- pageText.cpp
+++ pageText.cpp
@@ -12,6 +12,22 @@ pageText::pageText() {
 
 void pageText::resetVars() {
     position = 130;
+    if(month() == 12 && day() == 25) {
+        message = 1;
+        out = "Merry Christmas Kim!";
+    }
+    else if(month() == 8 && day() == 25) {
+        message = 2;
+        out = "Happy Birthday Kim!";
+    }
+    else if(month() == 6 && day() == 1) {
+        message = 3;
+        out = "Happy Anniversary Kim!";
+    }
+    else {
+        message = 0;
+        out = "Blake loves Kim";
+    }
 }
 
 void pageText::showPage(bool newPage) {
@@ -28,13 +44,26 @@ String pageText::getTitle() {
 void pageText::scrollText() {
     disp.drawBtnBar('<', "Menu", '>');
     display.setTextColor(WHITE);
+    //display.setCursor(0, 20);
+    //display.setTextSize(1);
+  //display.print(((42 * out.length()) + SSD1306_LCDWIDTH) * -1);
+    
+    
     display.setTextSize(7);
       position = position - 1;
   display.setCursor(position, 0);
-  display.println("Hello, world.");
+  display.print(out);
+  //if(month() == 12 && day() == 25) {
+  //    display.println("Merry Christmas Kim!");
+  //}
   display.display();
-  if(position < -650) {
-    position = 130;
+  int offset = ((42 * out.length()) + SSD1306_LCDWIDTH) * -1;
+  if(position < offset) {
+    position = SSD1306_LCDWIDTH;
   }
+  
+//    if(position < (42 * out.length()) + SSD1306_LCDWIDTH) {
+//    position = SSD1306_LCDWIDTH;
+//  }
   
 }
